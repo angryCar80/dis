f2595da4785f9ae75f635bc1756bd182ad5cac51
angryCar80 <ouabbasaymen@gmail.com>
2026-06-26 22:44:20 +0100

    squash! feat: this is the best

--- main.c
+++ main.c
@@ -1,13 +1,30 @@
+#include <dirent.h>
 #include <stdio.h>
 #include <string.h>
-#include <>
 
 int main(int argc, char *argv[]) {
   if (strcmp("--show", argv[1]) == 0) {
-    if (strcmp("all", argv[1]) == 0) {
-      // TODO CODE THE FUNCTION OF THIS
+    if (argc == 3) {
+      if (strcmp("all", argv[2]) == 0) {
+        DIR *dr;
+        struct dirent *en;
+
+        dr = opendir(".");
+        if (dr) {
+          while ((en = readdir(dr)) != NULL) {
+            printf("%s\n", en->d_name);
+          }
+          closedir(dr);
+        }
+        if (strcmp("costume", argv[2]) == 0) {
+
+        } else {
+          printf("Could not open directory\n");
+        }
+      }
     } else {
-      // TODO CODE THE FUNCTION OF THIS
+      printf("You Should Try --show all|custome\nfor more information try "
+             "`--help` argument\n");
     }
   } else if (strcmp("--help", argv[1]) == 0) {
     printf("This is The Help Menu\n");
